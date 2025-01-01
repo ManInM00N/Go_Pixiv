5f58b991bb0baef7541f16739515141054b48c71
ManInM00N <571404393@qq.com>
2025-01-02 01:26:20 +0800

    tidy package size

--- frontend/main.js
+++ frontend/main.js
@@ -3,14 +3,22 @@ import ElementPlus from 'element-plus'
 import router from './src/router/index.js'
 import 'element-plus/dist/index.css'
 import 'element-plus/theme-chalk/dark/css-vars.css'
-import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+// import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import {HomeFilled,StarFilled,Histogram,Search,Tools,Download} from '@element-plus/icons-vue'
 import 'element-plus/theme-chalk/base.css'
 import App from './src/App.vue'
 import * as config from "./src/assets/js/configuration.js"
 const app = createApp(App)
-for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-    app.component(key, component)
-}
+// for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+//     app.component(key, component)
+// }
+app.component("HomeFilled",HomeFilled)
+app.component("StarFilled",StarFilled)
+app.component("Histogram",Histogram)
+app.component("Search",Search)
+app.component("Tools",Tools)
+app.component("Download",Download)
+
 const resultElement = document.getElementById('result');
 app.use(router)
 app.use(ElementPlus)
